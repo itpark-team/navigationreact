885eef838489baa08edaab807c48ca8268867c8e
itpark <a.m.smyslov@yandex.ru>
2023-03-27 22:28:55 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import Home from "./components/Home";
 import About from "./components/About";
 import Info from "./components/Info";
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import {Outlet} from 'react-router-dom';
+import {Link} from "react-router-dom";
 
 const Layout = () => {
     return (
         <div>
-            
+            <div>
+                <Link to="/">Home</Link> |
+                <Link to="/info">Info</Link> |
+                <Link to="/about">About</Link>
+            </div>
+
+            <div>
+                <Outlet/>
+            </div>
+
+            <div>&copy; 2023</div>
         </div>
     );
 };
